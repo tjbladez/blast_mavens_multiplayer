42d16481ad2667661b869df88298c3b20845baa0
Nickolay Schwarz <tjbladez@gmail.com>
2011-02-15 13:18:17 -0600

    Adding bomb animation

--- lib/bomb.rb
+++ lib/bomb.rb
@@ -3,19 +3,20 @@ class Bomb
 
   attr_accessor :time_counter
   def initialize(x,y)
-    # puts "registering x: #{x}, y: #{y}, top_x:#{top_x}, top_y:#{top_y}"
     register!(x, y)
-    @t_size      = Processor::TileSize
-    @sprites     = Gosu::Image.load_tiles(Processor.window, "resources/images/dyno.png", @t_size, @t_size, false)
+    @t_size       = Processor::TileSize
+    @sprites      = Gosu::Image.load_tiles(Processor.window, "resources/images/dynoset.png", @t_size, @t_size, false)
     @time_counter = 60
+    @sprite_index = 0
   end
 
   def draw
-    @sprites.first.draw(top_x, top_y, 1)
+    @sprites[@sprite_index].draw(top_x, top_y, 1)
     update
   end
 
   def update
     @time_counter -= 1
+    @sprite_index += 1 if @time_counter % 15 == 0
   end
 end
