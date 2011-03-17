d20a5e87a6211f593e3f260a7804c6fc62ea6c03
Nickolay Schwarz <tjbladez@gmail.com>
2011-03-17 11:54:52 -0500

    Adding basic menu

--- lib/menu_window.rb
+++ lib/menu_window.rb
@@ -2,7 +2,8 @@ class MenuWindow < Gosu::Window
   def initialize
     super(*Processor::Screen)
     @pointer = Gosu::Image.new(self,"resources/images/cursor.png",true)
-    # @font = Gosu::Font.new(self, Gosu::DefautFont, true, 20)
+    @font    = Gosu::Font.new(self, Gosu::default_font_name, 20)
+
     @px = @py = 0
   end
 
@@ -12,14 +13,19 @@ class MenuWindow < Gosu::Window
 
   def draw
     @pointer.draw(@px,@py,0)
+    @font.draw('Start Game', 400, 300, 1)
+    @font.draw('Options', 400, 350, 1)
+    @font.draw('Exit', 400, 400, 1)
   end
 
   def mouse_clicked(x, y)
-    Processor.start_game
+    Processor.start_game if (380..480).include?(x) && (280..320).include?(y)
+    close if (380..480).include?(x) && (380..420).include?(y)
   end
 
   def button_down(id)
-    return Processor.start_game if id == Gosu::KbReturn
+    close if id == Gosu::KbEscape
+    Processor.start_game if id == Gosu::KbReturn
     mouse_clicked(@px, @py) if id == Gosu::MsLeft
   end
 end
