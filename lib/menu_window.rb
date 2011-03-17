4ebd5d8cb155651eb4e4bc24a649b029a3685b33
Nickolay Schwarz <tjbladez@gmail.com>
2011-03-17 11:32:07 -0500

    Adding a cursor for game window

--- lib/menu_window.rb
+++ lib/menu_window.rb
@@ -1,9 +1,25 @@
 class MenuWindow < Gosu::Window
   def initialize
     super(*Processor::Screen)
+    @pointer = Gosu::Image.new(self,"resources/images/cursor.png",true)
+    # @font = Gosu::Font.new(self, Gosu::DefautFont, true, 20)
+    @px = @py = 0
+  end
+
+  def update
+    @px, @py = mouse_x, mouse_y
+  end
+
+  def draw
+    @pointer.draw(@px,@py,0)
+  end
+
+  def mouse_clicked(x, y)
+    Processor.start_game
   end
 
   def button_down(id)
-    Processor.start_game if id == Gosu::KbReturn
+    return Processor.start_game if id == Gosu::KbReturn
+    mouse_clicked(@px, @py) if id == Gosu::MsLeft
   end
 end
